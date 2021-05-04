8f9997c40d0838c80f83b277bb5598abb6450578
Haspamelodica <melodicahaspa@gmail.com>
2021-05-04 10:47:57 +0200

    Implemented more instructions in AbstractMaMaInterpreter

--- MaMaAbstractInterpreter/src/net/haspamelodica/mama/interpreter/AbstractMaMaInterpreter.java
+++ MaMaAbstractInterpreter/src/net/haspamelodica/mama/interpreter/AbstractMaMaInterpreter.java
@@ -56,57 +56,154 @@ public class AbstractMaMaInterpreter
 		switch(instruction.getOpcode())
 		{
 			case add:
-				stack.pushBasic(stack.popBasic() + stack.popBasic());
+				add();
 				return true;
 			case mul:
-				stack.pushBasic(stack.popBasic() * stack.popBasic());
+				mul();
 				return true;
 			case loadc:
-				stack.pushBasic(instruction.getImmediate());
+				loadc(instruction.getImmediate());
 				return true;
 			case pushloc:
-				stack.pushHeapReference(stack.getHeapReferenceRelative(instruction.getImmediate()));
+				pushloc(instruction.getImmediate());
 				return true;
 			case pushglob:
-				stack.pushHeapReference(getGlobalPointer().checkVector().get(instruction.getImmediate()));
+				pushglob(instruction.getImmediate());
 				return true;
 			case slide:
-				HeapObject remainingValue = stack.popHeapReference();
-				stack.popMultiple(instruction.getImmediate());
-				stack.pushHeapReference(remainingValue);
+				slide(instruction.getImmediate());
+				return true;
+			case jump:
+				jump(instruction.getImmediate());
 				return true;
 			case mark:
-				stack.pushHeapReference(getGlobalPointer());
-				stack.pushBasic(getFramePointer());//TODO keep the information that this value represents a stack pointer
-				stack.pushBasic(instruction.getImmediate());//TODO keep the information that this value represents a code pointer
-				setFramePointer(stack.getStackPointer());
+				mark(instruction.getImmediate());
 				return true;
 			case apply:
-				Function calledFunction = stack.popHeapReference().checkFunction();
-				setGlobalPointer(calledFunction.getGlobalPointer());
-				setCodePointer(calledFunction.getCodePointer());
-				Vector arguments = calledFunction.getArgumentPointer().checkVector();
-				for(int i = 0; i < arguments.getLength(); i ++)
-					stack.pushHeapReference(arguments.get(i));
+				apply();
+				return true;
+			case targ:
+				targ(instruction.getImmediate());
+				return true;
+			case return_:
+				return_(instruction.getImmediate());
 				return true;
 			case getbasic:
-				stack.pushBasic(stack.popHeapReference().checkBasicValue().getValue());
+				getbasic();
 				return true;
 			case mkbasic:
-				stack.pushHeapReference(heap.createBasic(stack.popBasic()));
+				mkbasic();
 				return true;
 			case mkvec:
-				HeapObject[] referencedObjects = new HeapObject[instruction.getImmediate()];
-				for(int i = instruction.getImmediate() - 1; i >= 0; i --)
-					referencedObjects[i] = stack.popHeapReference();
-				stack.pushHeapReference(heap.createVector(List.of(referencedObjects)));
+				mkvec(instruction.getImmediate());
 				return true;
 			case mkfunval:
-				stack.pushHeapReference(heap.createFunction(instruction.getImmediate(), heap.createVector(List.of()), stack.popHeapReference()));
+				mkfunval(instruction.getImmediate());
 				return true;
 			default:
 				throw new InterpreterException("Unimplemented opcode: " + instruction.getOpcode());
 		}
+	}
+
+	private void add()
+	{
+		stack.pushBasic(stack.popBasic() + stack.popBasic());
+	}
+	private void mul()
+	{
+		stack.pushBasic(stack.popBasic() * stack.popBasic());
+	}
+	private void loadc(int value)
+	{
+		stack.pushBasic(value);
+	}
+	private void pushloc(int offsetFromStackPointer)
+	{
+		stack.pushHeapReference(stack.getHeapReferenceRelative(offsetFromStackPointer));
+	}
+	private void pushglob(int index)
+	{
+		stack.pushHeapReference(getGlobalPointer().checkVector().get(index));
+	}
+	private void slide(int numberOfValuesToPop)
+	{
+		HeapObject remainingValue = stack.popHeapReference();
+		stack.popMultiple(numberOfValuesToPop);
+		stack.pushHeapReference(remainingValue);
+	}
+	private void jump(int target)
+	{
+		setCodePointer(target);
+	}
+	private void mark(int returnAddress)
+	{
+		stack.pushHeapReference(getGlobalPointer());
+		stack.pushBasic(getFramePointer());//TODO keep the information that this value represents a stack pointer
+		stack.pushBasic(returnAddress);//TODO keep the information that this value represents a code pointer
+		setFramePointer(stack.getStackPointer());
+	}
+	private void apply()
+	{
+		Function calledFunction = stack.popHeapReference().checkFunction();
+		setGlobalPointer(calledFunction.getGlobalPointer());
+		setCodePointer(calledFunction.getCodePointer());
+		Vector arguments = calledFunction.getArgumentPointer().checkVector();
+		for(int i = 0; i < arguments.getLength(); i ++)
+			stack.pushHeapReference(arguments.get(i));
+	}
+	private void targ(int parameterCount)
+	{
+		int argumentCount = stack.getStackPointer() - getFramePointer();
+		if(argumentCount < parameterCount)
+		{
+			HeapObject argumentPointer = createVector(argumentCount);
+			stack.pushHeapReference(heap.createFunction(getCodePointer() - 1, argumentPointer, getGlobalPointer()));
+			popenv();
+		}
+	}
+	private void return_(int parameterCount)
+	{
+		if(stack.getStackPointer() - getFramePointer() == parameterCount + 1)
+			popenv();
+		else
+		{
+			slide(parameterCount);
+			apply();
+		}
+	}
+
+	private void getbasic()
+	{
+		stack.pushBasic(stack.popHeapReference().checkBasicValue().getValue());
+	}
+	private void mkbasic()
+	{
+		stack.pushHeapReference(heap.createBasic(stack.popBasic()));
+	}
+	private void mkvec(int valueCount)
+	{
+		stack.pushHeapReference(createVector(valueCount));
+	}
+	private void mkfunval(int bodyCodePointer)
+	{
+		stack.pushHeapReference(heap.createFunction(bodyCodePointer, heap.createVector(List.of()), stack.popHeapReference()));
+	}
+	private void popenv()
+	{
+		HeapObject returnValue = stack.popHeapReference();
+		stack.popMultiple(stack.getStackPointer() - getFramePointer());
+		setCodePointer(stack.popBasic());
+		setFramePointer(stack.popBasic());
+		setGlobalPointer(stack.popHeapReference());
+		stack.pushHeapReference(returnValue);
+	}
+
+	private HeapObject createVector(int valueCount)
+	{
+		HeapObject[] referencedObjects = new HeapObject[valueCount];
+		for(int i = valueCount - 1; i >= 0; i --)
+			referencedObjects[i] = stack.popHeapReference();
+		return heap.createVector(List.of(referencedObjects));
 	}
 
 	public int getCodePointer()
