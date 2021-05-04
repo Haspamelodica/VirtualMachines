5f6da20bb0f041259473ba9a687093834884657b
Haspamelodica <melodicahaspa@gmail.com>
2021-05-04 11:09:52 +0200

    Added an example letrec program

--- PuFCompiler/test/net/haspamelodica/puf/compiler/PuFCompilerPlayground.java
+++ PuFCompiler/test/net/haspamelodica/puf/compiler/PuFCompilerPlayground.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import net.haspamelodica.puf.compiler.environment.AddressEnvironment;
 import net.haspamelodica.puf.model.expressions.Binary;
+import net.haspamelodica.puf.model.expressions.Conditional;
 import net.haspamelodica.puf.model.expressions.Binary.BinaryOp;
+import net.haspamelodica.puf.model.expressions.Letrec.VariableDeclaration;
 import net.haspamelodica.puf.model.expressions.Expression;
 import net.haspamelodica.puf.model.expressions.FunctionApplication;
 import net.haspamelodica.puf.model.expressions.FunctionDefinition;
 import net.haspamelodica.puf.model.expressions.IntConstant;
 import net.haspamelodica.puf.model.expressions.Let;
+import net.haspamelodica.puf.model.expressions.Letrec;
 import net.haspamelodica.puf.model.expressions.Variable;
 
 public class PuFCompilerPlayground
@@ -42,6 +45,16 @@ public class PuFCompilerPlayground
 		assembler = new Assembler();
 		largerExample.appendCodeV(assembler, new AddressEnvironment(), 0, true);
 		System.out.println("--- larger example:");
+		System.out.println(assembler.assemble());
+
+		Expression letrecExample = new Letrec(List.of(new VariableDeclaration("f", new FunctionDefinition(List.of("x", "y"),
+				new Conditional(new Binary(new Variable("y"), BinaryOp.LESS_OR_EQUAL, new IntConstant(1)), new Variable("x"),
+						new FunctionApplication(new Variable("f"), List.of(new Binary(new Variable("x"), BinaryOp.TIMES, new Variable("y")),
+								new Binary(new Variable("y"), BinaryOp.MINUS, new IntConstant(1)))))))),
+				new FunctionApplication(new Variable("f"), List.of(new IntConstant(1))));
+		assembler = new Assembler();
+		letrecExample.appendCodeV(assembler, new AddressEnvironment(), 0, true);
+		System.out.println("--- letrec example:");
 		System.out.println(assembler.assemble());
 	}
 }
